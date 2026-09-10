671e4d5af14621b7bd1b7c7fee0b6415ea09a841
Bibek-M <bibek7020u@gmail.com>
2026-09-10 11:25:04 +0530

    Home Page Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import HomeNav from '../components/HomeNav'
+import StayInformed from '../components/StayInformed'
+const Home = () => {
+  return (
+    <div>
+        <h1><HomeNav/></h1>
+        <StayInformed/>
+    </div>
+  )
+}
+
+export default Home
